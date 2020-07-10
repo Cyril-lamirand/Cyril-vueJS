5f7554faf7188d6cc81d42fca1747ce4e3a897a4
Cyril Lamirand @DEV <Contact.cyril1@gmail.com>
2020-07-10 09:49:39 +0200

    Update Portfolio.vue

--- src/components/Portfolio.vue
+++ src/components/Portfolio.vue
@@ -1,7 +1,55 @@
 <template>
     <div class="container">
-        <h1>Hello Porfolio</h1>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto atque dolorem, dolorum explicabo illum iure iusto laudantium nemo officia quia, sapiente sit? Iure porro quaerat tempora veniam veritatis. Deserunt, odio.</p>
+        <b-carousel
+                id="carousel-1"
+                v-model="slide"
+                :interval="4000"
+                controls
+                indicators
+                background="#ababab"
+                img-width="1024"
+                img-height="480"
+                style="text-shadow: 1px 1px 2px #333;"
+                @sliding-start="onSlideStart"
+                @sliding-end="onSlideEnd"
+        >
+            <!-- Text slides with image -->
+            <b-carousel-slide
+                    caption="First slide"
+                    text="Nulla vitae elit libero, a pharetra augue mollis interdum."
+                    img-src="https://picsum.photos/1024/480/?image=52"
+            ></b-carousel-slide>
+
+            <!-- Slides with custom text -->
+            <b-carousel-slide img-src="https://picsum.photos/1024/480/?image=54">
+                <h1>Hello world!</h1>
+            </b-carousel-slide>
+
+            <!-- Slides with image only -->
+            <b-carousel-slide img-src="https://picsum.photos/1024/480/?image=58"></b-carousel-slide>
+
+            <!-- Slides with img slot -->
+            <!-- Note the classes .d-block and .img-fluid to prevent browser default image alignment -->
+            <b-carousel-slide>
+                <template v-slot:img>
+                    <img
+                            class="d-block img-fluid w-100"
+                            width="1024"
+                            height="480"
+                            src="https://picsum.photos/1024/480/?image=55"
+                            alt="image slot"
+                    >
+                </template>
+            </b-carousel-slide>
+
+            <!-- Slide with blank fluid image to maintain slide aspect ratio -->
+            <b-carousel-slide caption="Blank Image" img-blank img-alt="Blank image">
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse eros felis, tincidunt
+                    a tincidunt eget, convallis vel est. Ut pellentesque ut lacus vel interdum.
+                </p>
+            </b-carousel-slide>
+        </b-carousel>
     </div>
 
 </template>
@@ -13,5 +61,7 @@
 </script>
 
 <style scoped>
+
+    .container{ padding-top: 60px;}
 
 </style>
